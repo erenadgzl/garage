d7c91097e7310df920886c2bd7c418dcfa84f30e
Eren Adıgüzel <erenadiguzel96@gmail.com>
2022-08-15 17:45:31 +0300

    Initial commit

---
+++ src/main/java/com/kafein/garage/GarageApplication.java
@@ -0,0 +1,13 @@
+package com.kafein.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageApplication.class, args);
+    }
+
+}
